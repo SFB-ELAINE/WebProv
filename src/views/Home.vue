8d9f96733f65ee98f9167b70af12e6b5edca8066
Jacob Smith <jsmith@max.informatik.uni-rostock.de>
2019-05-27 14:40:00 +0200

    creating simple d3 example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div id="chart"></div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import * as d3 from 'd3';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
@@ -14,5 +14,45 @@ export default Vue.extend({
   components: {
     HelloWorld,
   },
+  mounted() {
+    interface Data {
+      label: string;
+      count: number;
+    };
+    var dataset: Data[] = [
+      { label: 'Abulia', count: 10 },
+      { label: 'Betelgeuse', count: 20 },
+      { label: 'Cantaloupe', count: 30 },
+      { label: 'Dijkstra', count: 40 }
+    ];
+
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;
+
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+    
+    var arc = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    var pie = d3.pie<Data>()
+      .value(d => d.count)
+      .sort(null);
+
+    var path = svg.selectAll('path')
+      .data(pie(dataset))
+      .enter()
+      .append('path')
+      .attr('d', <any>arc)
+      .attr('fill', (d, i) => color(d.data.label));
+  }
 });
 </script>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicPath: '/WebProv/',
+    outputDir: 'docs'
+}
