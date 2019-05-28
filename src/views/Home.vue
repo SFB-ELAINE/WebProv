7154a46ef41a91f7dc1600620292ed16982495c6
Jacob Smith <jacob.smith@unb.ca>
2019-05-28 14:54:00 +0200

    basic graph now visualizing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
-    <div id="chart"></div>
+    <svg ref="svg" :height="height" :width="width"></svg>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import * as d3 from 'd3';
+import data from '@/assets/miserables';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
@@ -14,45 +15,86 @@ export default Vue.extend({
   components: {
     HelloWorld,
   },
+  data: () => ({
+    height: 600,
+    width: 600,
+  }),
   mounted() {
-    interface Data {
-      label: string;
-      count: number;
+    const nodes = data.nodes.map((n) => ({
+      ...n,
+      index: 0, // this is useless but it gets rid of a type error
+    }));
+    const links = data.links.map((l) => ({ ...l }));
+
+    const simulation = d3.forceSimulation(nodes)
+      // @ts-ignore
+      .force('link', d3.forceLink(links).id((d) => d.id))
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(this.width / 2, this.height / 2));
+
+    const svg = d3.select(this.$refs.svg as Element);
+
+    const link = svg.append('g')
+      .attr('stroke', '#999')
+      .attr('stroke-opacity', 0.6)
+      .selectAll('line')
+      .data(links)
+      .join('line')
+      .attr('stroke-width', (d) => Math.sqrt(d.value));
+
+    const drag = (simulation: ReturnType<typeof d3.forceSimulation>) => {
+
+      function dragstarted(d: d3.SimulationNodeDatum) {
+        if (!d3.event.active) { simulation.alphaTarget(0.3).restart(); }
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+
+      function dragged(d: d3.SimulationNodeDatum) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+      }
+
+      function dragended(d: d3.SimulationNodeDatum) {
+        if (!d3.event.active) { simulation.alphaTarget(0); }
+        d.fx = null;
+        d.fy = null;
+      }
+
+      return d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended);
     };
-    var dataset: Data[] = [
-      { label: 'Abulia', count: 10 },
-      { label: 'Betelgeuse', count: 20 },
-      { label: 'Cantaloupe', count: 30 },
-      { label: 'Dijkstra', count: 40 }
-    ];
 
-    var width = 360;
-    var height = 360;
-    var radius = Math.min(width, height) / 2;
+    const scale = d3.scaleOrdinal(d3.schemeCategory10);
+    const node = svg.append('g')
+      .attr('stroke', '#fff')
+      .attr('stroke-width', 1.5)
+      .selectAll('circle')
+      .data(nodes)
+      .join('circle')
+      .attr('r', 5)
+      .attr('fill', (d) => scale(d.group + ''))
+      .call(drag(simulation as any) as any);
 
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+    simulation.on('tick', () => {
+      link
+        // @ts-ignore
+        .attr('x1', (d) => d.source.x)
+        // @ts-ignore
+        .attr('y1', (d) => d.source.y)
+        // @ts-ignore
+        .attr('x2', (d) => d.target.x)
+        // @ts-ignore
+        .attr('y2', (d) => d.target.y);
 
-    var svg = d3.select('#chart')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
-      .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
-    
-    var arc = d3.arc()
-      .innerRadius(0)
-      .outerRadius(radius);
-
-    var pie = d3.pie<Data>()
-      .value(d => d.count)
-      .sort(null);
-
-    var path = svg.selectAll('path')
-      .data(pie(dataset))
-      .enter()
-      .append('path')
-      .attr('d', <any>arc)
-      .attr('fill', (d, i) => color(d.data.label));
-  }
+      node
+        // @ts-ignore
+        .attr('cx', (d) => d.x)
+        // @ts-ignore
+        .attr('cy', (d) => d.y);
+    });
+  },
 });
 </script>
