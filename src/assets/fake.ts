76c9786cd00697147f9ae552dbae6e88d5aba788
Jacob Smith <jacob.smith@unb.ca>
2019-05-29 09:28:01 +0200

    drawing with fake data now

---
+++ src/assets/fake.ts
@@ -0,0 +1,129 @@
+type NodeType =
+  'model' |
+  'wet-lab data' |
+  'simulation data' |
+  'model-building-activity' |
+  'model exploration activity';
+
+interface Node {
+  type: NodeType;
+  id: string;
+  text: string;
+}
+
+interface Link {
+  source: string;
+  target: string;
+}
+
+const nodes: Node[] = [
+  {
+    type: 'wet-lab data',
+    id: 'W12_1',
+    text: 'W12_1',
+  },
+  {
+    type: 'wet-lab data',
+    id: 'Wx_1',
+    text: 'Wx_1',
+  },
+  {
+    type: 'wet-lab data',
+    id: 'Wx_2',
+    text: 'Wx_2',
+  },
+  {
+    type: 'wet-lab data',
+    id: 'W12_2',
+    text: 'W12_2',
+  },
+  {
+    type: 'wet-lab data',
+    id: 'W12_3',
+    text: 'W12_3',
+  },
+  {
+    type: 'wet-lab data',
+    id: 'Wx_3',
+    text: 'Wx_3',
+  },
+  {
+    type: 'model-building-activity',
+    id: 'MBA_1',
+    text: 'MBA_1',
+  },
+  {
+    type: 'model-building-activity',
+    id: 'MBA_2',
+    text: 'MBA_2',
+  },
+  {
+    type: 'simulation data',
+    id: 'S12_1',
+    text: 'S12_1',
+  },
+  {
+    type: 'simulation data',
+    id: 'S12_2',
+    text: 'S12_2',
+  },
+  {
+    type: 'model',
+    id: 'M12',
+    text: 'M12 (Haack et al., PLoS comp. bio. 2015)',
+  },
+  {
+    type: 'model',
+    id: 'M12\'',
+    text: 'M12\' (Haack et al., PLoS comp. bio. 2015)',
+  },
+];
+
+const links: Link[] = [
+  {
+    source: 'MBA_1',
+    target: 'M12',
+  },
+  {
+    source: 'MBA_1',
+    target: 'Wx_2',
+  },
+  {
+    source: 'MBA_1',
+    target: 'Wx_1',
+  },
+  {
+    source: 'S12_1',
+    target: 'MBA_1',
+  },
+  {
+    source: 'M12',
+    target: 'MBA_1',
+  },
+  {
+    source: 'MBA_2',
+    target: 'M12',
+  },
+  {
+    source: 'MBA_2',
+    target: 'W12_2',
+  },
+  {
+    source: 'MBA_2',
+    target: 'W12_3',
+  },
+  {
+    source: 'MBA_2',
+    target: 'Wx_3',
+  },
+  {
+    source: 'M12\'',
+    target: 'MBA_2',
+  },
+];
+
+
+export default {
+  nodes,
+  links,
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import * as d3 from 'd3';
-import data from '@/assets/miserables';
+import data from '@/assets/fake';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
@@ -18,6 +18,7 @@ export default Vue.extend({
   data: () => ({
     height: 600,
     width: 600,
+    size: 40,
   }),
   mounted() {
     const nodes = data.nodes.map((n) => ({
@@ -33,6 +34,20 @@ export default Vue.extend({
       .force('center', d3.forceCenter(this.width / 2, this.height / 2));
 
     const svg = d3.select(this.$refs.svg as Element);
+    // svg.style
+
+    svg.append('svg:defs').selectAll('marker')
+        .data(['end'])  // Different link/path types can be defined here
+        .enter().append('svg:marker')  // This section adds in the arrows
+        .attr('id', String)
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 10)
+        .attr('refY', 0)
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('orient', 'auto')
+        .append('svg:path')
+        .attr('d', 'M0,-5L10,0L0,5');
 
     const link = svg.append('g')
       .attr('stroke', '#999')
@@ -40,9 +55,10 @@ export default Vue.extend({
       .selectAll('line')
       .data(links)
       .join('line')
-      .attr('stroke-width', (d) => Math.sqrt(d.value));
+      .attr('stroke-width', (d) => Math.sqrt(3))
+      .attr('marker-end', 'url(#end)');
 
-    const drag = (simulation: ReturnType<typeof d3.forceSimulation>) => {
+    const drag = () => {
 
       function dragstarted(d: d3.SimulationNodeDatum) {
         if (!d3.event.active) { simulation.alphaTarget(0.3).restart(); }
@@ -68,15 +84,32 @@ export default Vue.extend({
     };
 
     const scale = d3.scaleOrdinal(d3.schemeCategory10);
-    const node = svg.append('g')
+    const g = svg.append('g')
       .attr('stroke', '#fff')
       .attr('stroke-width', 1.5)
-      .selectAll('circle')
+      .selectAll('.node')
       .data(nodes)
-      .join('circle')
-      .attr('r', 5)
-      .attr('fill', (d) => scale(d.group + ''))
-      .call(drag(simulation as any) as any);
+      .join('g')
+      .attr('class', 'node')
+      // @ts-ignore
+      .call(drag());
+
+    const node = g
+      .append('rect')
+      .attr('width', (d) => d.text.length * 8 + 10) // 14 just kinda works well
+      .attr('height', this.size)
+      .attr('fill', (d) => scale(1 + ''));
+
+    g.append('text')
+      // @ts-ignore
+      .attr('x', (d) => d.text.length * 4 + 5) // duplicate from above
+      .attr('y', () => this.size / 2)
+      .attr('stroke', 'white')
+      .style('', '')
+      .style('font-family', 'monospace')
+      .style('user-select', 'none')
+      .style('text-anchor', 'middle')
+      .text((d) => d.text);
 
     simulation.on('tick', () => {
       link
@@ -89,11 +122,9 @@ export default Vue.extend({
         // @ts-ignore
         .attr('y2', (d) => d.target.y);
 
-      node
+      g
         // @ts-ignore
-        .attr('cx', (d) => d.x)
-        // @ts-ignore
-        .attr('cy', (d) => d.y);
+        .attr('transform', (d) => `translate(${d.x}, ${d.y})`);
     });
   },
 });
