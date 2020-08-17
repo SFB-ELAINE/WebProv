149443c099b366a637eff5001870ad44b99814d0
kbudde <kaibudde@gmail.com>
2020-08-17 09:51:23 +0200

    renamed 'InformationField' to 'Further Information'

--- packages/frontend/src/components/NodeFormCard.vue
+++ packages/frontend/src/components/NodeFormCard.vue
@@ -34,7 +34,7 @@
     </b-field>
 
 
-    <b-field label="InformationField" style="flex-direction: column; align-items: flex-start;">
+    <b-field label="Further Information" style="flex-direction: column; align-items: flex-start;">
       <div v-for="(field, j) in fields" :key="j" style="display: flex">
         <b-field>
           <b-input placeholder="Key" :value="field.key" @input="updateKey(j, $event)"></b-input>
