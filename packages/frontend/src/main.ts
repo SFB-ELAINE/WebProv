c61498b88bba0f5acb073decef2944f320620ed8
Jacob Smith <jacob.smith@unb.ca>
2021-01-16 16:46:06 -0400

    sentry

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -3,6 +3,21 @@ import App from '@/App.vue';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 import VueCompositionApi from '@vue/composition-api';
+import { Integrations } from '@sentry/tracing';
+import * as Sentry from '@sentry/vue';
+
+Sentry.init({
+  Vue,
+  dsn: 'https://e198b0768d3d4ee0a9ad68787f483dad@o373147.ingest.sentry.io/5595433',
+  autoSessionTracking: true,
+  integrations: [
+    new Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 Vue.config.productionTip = false;
 
